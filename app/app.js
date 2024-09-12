5b0aab269948fc465699ae58580cd40a7b479aa1
dfwpekas <chiemerienwafor88@gmail.com>
2024-09-12 17:07:41 +0100

    chore: configure error handling middlewares in the Express app
    - Import `notFoundErr` and `globalErrHandler` middlewares from `globalErrHandler`.
    - Apply `notFoundErr` middleware to handle 404 errors.
    - Apply `globalErrHandler` middleware to handle and format all other errors.
    - Export the configured Express app.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const {globalErrHandler, notFoundErr} = require("../middleware/globalErrHandler");
 
+
+app.use(notFoundErr);
+app.use(globalErrHandler);
 
 module.exports = app;
